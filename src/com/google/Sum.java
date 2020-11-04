306970cc506675150cb1716652e6d5f8e83942e9
andriy999000 <vladRydenko77Agmail.com>
2020-11-04 21:35:47 +0200

    Initial commit

---
+++ src/com/google/Sum.java
@@ -0,0 +1,10 @@
+package com.google;
+
+public class Sum {
+    public static int work(int v1,int v2) {
+        return v1 + v2;
+    }
+    public static double work(double v1,double v2){
+        return v1 + v2;
+    }
+}
---
+++ src/com/google/Test.java
@@ -0,0 +1,8 @@
+package com.google;
+import static com.google.Sum.work;
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(work(1,2));
+
+    }
+}
